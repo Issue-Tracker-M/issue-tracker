ffca57a6397b520acf5f80bd620bd352b29c71a7
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2021-01-26 16:59:25 +0500

    feat(api): a JSONify utility type
    To be used to transform mongoose types to json types to the same shape
    they get transformed once sent as JSON response

--- packages/api/src/utils/typeUtils.ts
+++ packages/api/src/utils/typeUtils.ts
@@ -1,5 +1,26 @@
-import { Document } from "mongoose";
+import { Document, Types } from "mongoose";
+/**
+ * @private
+ * Excludes fields specific to mongoose documents from a type
+ */
 export type JSONed<T> = Pick<
   T,
   Exclude<keyof T, Exclude<keyof Document, "_id">>
 >;
+
+/**
+ * @private
+ * Recursively applies itself to mongoose documents
+ * Intended to represent transformation from a mongoose document to JSON object
+ */
+export type JSONify<T> = T extends Types.ObjectId
+  ? string
+  : {
+      [P in keyof JSONed<T>]: T[P] extends Types.ObjectId
+        ? string
+        : T[P] extends Types.Array<infer K>
+        ? JSONify<K>[]
+        : T[P] extends Document
+        ? JSONify<T[P]>
+        : T[P];
+    };
