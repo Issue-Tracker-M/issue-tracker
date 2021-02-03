e02e72098b0cf11590b2d2f367f9e67fe786511b
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2021-02-03 17:24:43 +0500

    fixup! fixed storybook to work with emotion 11

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,9 +1,23 @@
 const path = require("path");
+const { DllReferencePlugin } = require("webpack");
+
 const toPath = (_path) => {
   // throw new Error(process.env.PWD + " " + process.cwd());
   return path.join(process.cwd(), _path);
 };
 const emotionPath = toPath("../../../node_modules/@emotion/react");
+
+// make a shallow copy of an object, rejecting keys that match /emotion/
+function emotionless(object, predicate) {
+  let result = {};
+  for (key in object) {
+    if (!/emotion/.test(key)) {
+      result[key] = object[key];
+    }
+  }
+  return result;
+}
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
@@ -12,14 +26,28 @@ module.exports = {
       ...config,
       resolve: {
         ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-          "@emotion/core": toPath("../../../node_modules/@emotion/react"),
-          "emotion-theming": toPath("../../../node_modules/@emotion/react"),
-          // "@emotion/core": emotionPath,
-          // "emotion-theming": emotionPath,
-        },
+        alias: emotionless(config.resolve.alias),
+        // alias: {
+        //   ...config.resolve.alias,
+        //   "@emotion/core": toPath("../../../node_modules/@emotion/react"),
+        //   "emotion-theming": toPath("../../../node_modules/@emotion/react"),
+        //   // "@emotion/core": emotionPath,
+        //   // "emotion-theming": emotionPath,
+        // },
       },
+      plugins: config.plugins.map((plugin) => {
+        // clone the config of the DllReferencePlugin and remove references to emotion libs
+        if (plugin instanceof DllReferencePlugin) {
+          const { name, content } = require(plugin.options.manifest);
+          return new DllReferencePlugin({
+            context: plugin.options.context,
+            name,
+            content: emotionless(content),
+          });
+        } else {
+          return plugin;
+        }
+      }),
     };
   },
 };
