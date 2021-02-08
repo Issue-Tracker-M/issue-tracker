20c6cb22ddaaf6319658f99277d457b8f778d035
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2021-02-08 17:12:04 +0500

    feat(webapp): home page

--- packages/webapp/src/pages/Home.tsx
+++ packages/webapp/src/pages/Home.tsx
@@ -1,78 +1,14 @@
-import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
-import { Link as RouterLink } from "react-router-dom";
-import {
-  Box,
-  Flex,
-  Heading,
-  IconButton,
-  useDisclosure,
-  VStack,
-  Button,
-  Divider,
-  HStack,
-  Link,
-  Center,
-} from "@chakra-ui/react";
-import { motion } from "framer-motion";
-import React from "react";
+import { Center, Heading } from "@chakra-ui/react";
+import React, { FC } from "react";
+import { AppLayout } from "../components/Layout/AppLayout";
 
-const DrawerNav = () => {
-  const { isOpen, onToggle } = useDisclosure(); /* Control panel opening */
+const Home: FC = () => {
   return (
-    <>
-      <motion.div
-        animate={{
-          height: "100vh",
-          width: isOpen ? "300px" : "0",
-        }}
-        style={{ overflow: "hidden" }}
-        initial={{ width: "0px" }}>
-        <HStack
-          color="black"
-          bg="white"
-          rounded="md"
-          h="100vh"
-          w="100%"
-          justify="space-between"
-          align="flex-start"
-          overflowY="auto">
-          <VStack w="100%">
-            <Center direction="row" justify="space-between" width="inherit  ">
-              <Heading>Logo</Heading>
-              <IconButton
-                aria-label="Close sidebar"
-                onClick={onToggle}
-                onFocus={() => (isOpen ? null : onToggle())}
-                icon={<CloseIcon />}
-              />
-            </Center>
-            <Link as={RouterLink} to="/home">
-              Content
-            </Link>
-          </VStack>
-          <Divider orientation="vertical" />
-        </HStack>
-      </motion.div>
-      {!isOpen && (
-        <IconButton
-          position="absolute"
-          top="1rem"
-          left="1rem"
-          aria-label="Open Nav menu"
-          icon={<HamburgerIcon />}
-          onClick={onToggle}
-        />
-      )}
-    </>
-  );
-};
-
-const Home = () => {
-  return (
-    <Box display="flex" flexDir="row">
-      <DrawerNav />
-      <Heading width="100%">CONTENT</Heading>
-    </Box>
+    <AppLayout>
+      <Center height="100%" width="100%">
+        <Heading>Welcome to issue tracker! 🚀</Heading>
+      </Center>
+    </AppLayout>
   );
 };
 
