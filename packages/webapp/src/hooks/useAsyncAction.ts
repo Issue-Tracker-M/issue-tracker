0122f2d5d4a20951b7312e3e7f810d0565017bba
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2021-02-10 16:57:20 +0500

    feat(webapp): useAsyncThunk custom hook

--- packages/webapp/src/hooks/useAsyncAction.ts
+++ packages/webapp/src/hooks/useAsyncAction.ts
@@ -1,22 +1,48 @@
-import { useEffect, useState } from 'react'
+import { AsyncThunk } from "@reduxjs/toolkit";
+import { useEffect, useState } from "react";
+import { useThunkDispatch } from "./useThunkDispatch";
+
+interface useAsyncThunkReturn {
+  /**
+   * Shows whether the thunk is pending or not
+   */
+  loading: boolean;
+  /**
+   * Error returned by the thunk
+   */
+  error: unknown;
+}
+
 /**
- * Exposes the state of the promise of the given async action, intended to be used for loading indications
- * meaningful state changes are meant to be handled within the action.
- * @param action async action to be used in the effect
+ * Exposes the state of the promise of the given async thunk, intended to be used for loading indications
+ * meaningful state changes are meant to be handled within the thunk.
+ * @param thunk async thunk to be used in the effect
+ * @param args args object to be passed to thunk
+ * @param condition if passed, will be called to decide whether the thunk is dispatched
  */
-export default function useAsyncAction(action: any) {
-  const [loading, setLoading] = useState(false)
+export default function useAsyncThunk<T extends AsyncThunk<any, any, any>>(
+  thunk: T,
+  args: Parameters<T>["0"],
+  condition?: (...args: unknown[]) => boolean
+): useAsyncThunkReturn {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const dispatch = useThunkDispatch();
 
   useEffect(() => {
-    let mounted = true
-    setLoading(true)
-    action().finally(() => {
-      if (mounted) setLoading(false)
-    })
-    return () => {
-      mounted = false
+    let mounted = true;
+    if (loading) return;
+    if (!condition || condition()) {
+      setLoading(true);
+      dispatch(thunk(args)).then((res) => {
+        if (res.meta.requestStatus === "rejected") setError(res.payload);
+        setLoading(false);
+      });
     }
-  }, [action])
+    return () => {
+      mounted = false;
+    };
+  }, [args, condition, dispatch, loading, thunk]);
 
-  return loading
+  return { loading, error };
 }
