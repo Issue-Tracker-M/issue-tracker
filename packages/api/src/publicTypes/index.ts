32b6088f80279f9763e9d73bfb3e8d1c8c4e8943
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2021-03-03 10:26:43 +0500

    chore(webapp): added ts build step to build script

--- packages/api/src/publicTypes/index.ts
+++ packages/api/src/publicTypes/index.ts
@@ -1,4 +1,10 @@
 import { InvitationTokenPopulatedDocument } from "../components/auth/models/InvitationToken";
+import { UserDocument } from "../components/users/model";
 import { JSONify } from "../utils/typeUtils";
 
 export type getInviteData = JSONify<InvitationTokenPopulatedDocument>;
+
+export type AuthResponse = {
+  token: string;
+  user: JSONify<UserDocument>;
+};
